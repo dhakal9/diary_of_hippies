fb704e8d1182a1d4ebb367563202b680e3125342
dhakal9 <dhakalamrit19@gmail.com>
2026-02-21 18:54:59 +0545

    test

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-python3 -m pip install -r requirements.txt --noinput
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput --clear 
+pip install -r requirements.txt --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear 
 
