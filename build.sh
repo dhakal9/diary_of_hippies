8a329a12f90fd5d27d54dc2f51d3d15bb21b0cb4
dhakal9 <dhakalamrit19@gmail.com>
2026-02-21 18:29:12 +0545

    vercel configuration updated by bikash dada

---
+++ build.sh
@@ -0,0 +1,5 @@
+python3 -m pip install -r requirements.txt --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput --clear 
+
