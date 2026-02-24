d15ded456d3473038faa3f9f430494df6445e627
dhakal9 <dhakalamrit19@gmail.com>
2026-02-24 11:47:52 +0545

    hello

--- api/index.py
+++ api/index.py
@@ -1,14 +1,9 @@
 import os
 import sys
 
-
-# Add project root to path
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.join(BASE_DIR, 'core')
 sys.path.append(BASE_DIR)
 
-# Set settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 from django.core.wsgi import get_wsgi_application
